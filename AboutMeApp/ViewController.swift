66f2598ac5c9ee3eb06c5a5e1a0231b9a4e514b8
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2023-11-23 22:57:51 +0300

    Initial Commit

---
+++ AboutMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Егор Аблогин on 23.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
